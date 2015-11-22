169ddca0729fac42ada40a690b4b6ba77e1c6224
unknown <tomer@TomerW81.HQ.LeadSpace.com>
2015-11-22 20:36:41 +0200

    Method signature fixed

--- FindMax.java
+++ FindMax.java
@@ -10,19 +10,17 @@ public class FindMax {
         //Define array of 10 numbers
         int myNumbers[] = new int[]{10, 20, 30, 40, 50, 45, 35, 25, 15, 5};
 
-        //Assign the array element to a parameter
-        int findMax = myNumbers[0];
-
         // Calling methods
-       int findMaxFor = findMaxFor(findMax, myNumbers);
-       int findMaxWhile = findMaxWhile(findMax, myNumbers);
+       int findMaxFor = findMaxFor(myNumbers);
+       int findMaxWhile = findMaxWhile(myNumbers);
 
        // Printing the results
           System.out.println("The largest number in the array is: " + findMaxFor + " --> Using For");
           System.out.println("The largest number in the array is: " + findMaxWhile + " --> Using While");
     }
 
-    public static int findMaxFor(int findMax, int myNumbers[]) {
+    public static int findMaxFor(int myNumbers[]) {
+        int findMax = myNumbers[0];
         // I am stating at 1 because findMax is already set to the first element
         for(int i = 1 ; i < myNumbers.length ; i++){
             if(myNumbers[i] > findMax){
@@ -32,8 +30,8 @@ public class FindMax {
         return findMax;
     }
 
-    public static int findMaxWhile(int findMax, int myNumbers[]) {
-
+    public static int findMaxWhile(int myNumbers[]) {
+        int findMax = myNumbers[0];
         // I am stating at 1 because findMax is already set to the first element
         int counter = 1;
         while(counter < myNumbers.length) {
